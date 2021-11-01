719fb48be5876b061413283efe67334bad1eb9f6
Ivan <ivan.jump@globo.com>
2021-10-31 23:11:19 -0300

    feature: card cart with basic styled component

---
+++ src/components/CardCart/index.tsx
@@ -0,0 +1,43 @@
+import { useCart } from "../../Provider/Cart/index";
+import { Container } from "./styles";
+
+interface ProductData {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+}
+
+interface ProductsProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  product: ProductData;
+}
+
+export const CardCart = ({
+  id,
+  title,
+  description,
+  image,
+  price,
+  product,
+}: ProductsProps) => {
+  const { deleteProduct } = useCart();
+
+  return (
+    <Container>
+      <img src={image} alt={title} height="150px" />
+      <h5>{title}</h5>
+      <p>
+        Price: <span>R$ {price}</span>
+      </p>
+      <button onClick={() => deleteProduct(product)}>Remover Produto</button>
+    </Container>
+  );
+};
+
+export default CardCart;
---
+++ src/components/CardCart/styles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 390px;
+  margin: 10px auto;
+  width: 280px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f1fffa;
+  color: #e3170a;
+  border-radius: 15px;
+  img {
+    margin: 10px auto;
+    height: 200px;
+  }
+  h5 {
+    margin: 5px auto;
+    width: 90%;
+    color: #493b2a;
+  }
+  p {
+    margin: 2px auto 10px;
+    width: 90%;
+    span {
+      color: #832161;
+    }
+  }
+  button {
+    width: 90%;
+    height: 30px;
+    padding: 5px 0;
+    margin-bottom: 5px;
+    background-color: #e3170a;
+    font-size: 18px;
+    color: #f1fffa;
+    border: 1px solid transparent;
+    border-radius: 5px;
+    &:hover {
+      background-color: #27435e;
+    }
+  }
+  @media (min-width: 768px) {
+    height: 320px;
+    margin: 5px auto;
+    img {
+      height: 130px;
+    }
+  }
+`;
